aaf6ad64c5034a3ca6633512ea14ab99491a0912
Joey Wong <370318843@qq.com>
2022-05-18 17:49:10 +0800

    new函数添加构造函数返回object和function情况逻辑

--- new.js
+++ new.js
@@ -1,21 +1,19 @@
 //new实现
-function Person(name, age) {
-  this.name = name
-  this.age = age
+function myTypeOf(obj) {
+  return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase()
+}
 
-}
-Person.prototype.say = () => {
-  console.log('I am a person')
-}
 const myNew = (constructor, ...args) => {
   if (typeof constructor !== 'function') {
     throw ("the first parameter is not a constructor function")
   }
   let instance = Object.create(constructor.prototype) // 原型指向constructor.prototype
-  constructor.apply(instance, args)
+
+  //如果constructor返回值是object或者function，那么new返回constructor返回值
+  const result = constructor.apply(instance, args)  // this指向instance
+  const resultType = myTypeOf(result)
+  if (resultType === "function" || resultType === "object") {
+    return result
+  }
   return instance
 }
-
-let joey=myNew(Person, 'joey wong', '26')
-console.log(joey)
-joey.say()
