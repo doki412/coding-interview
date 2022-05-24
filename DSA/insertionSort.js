2dc8763f1ca60d2b2a8c221654361ff90f62c650
Joey Wong <370318843@qq.com>
2022-05-24 11:43:35 +0800

    插入排序优化

--- DSA/insertionSort.js
+++ DSA/insertionSort.js
@@ -1,5 +1,5 @@
-//插入排序 O(n*n)
-const InsertionSort = (arr, n) => {
+//插入排序 O(n*n) 在近乎有序的数组中排序很高效
+const InsertionSort1 = (arr, n) => {
   for (let i = 1; i < arr.length; i++) {
 
     //寻找元素arr[i]合适的插入位置
@@ -8,5 +8,17 @@ const InsertionSort = (arr, n) => {
       arr[j] = arr[j - 1]
       arr[j - 1] = temp
     }
+  }
+}
+
+//优化版
+const InsertionSort2 = (arr, n) => {
+  for (let i = 1; i < arr.length; i++) {
+    let e = arr[i]
+    let j;
+    for (j = i; j > 0 && arr[j - 1] > e; j) {
+      arr[j] = arr[j - 1]
+    }
+    arr[j] = e
   }
 }
